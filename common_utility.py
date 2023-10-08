fa9499d08f6530f1c311c37381e4b1028b1c13fd
Maria Uzun <uzunmasha@gmail.com>
2023-10-08 12:35:02 +0300

    Add run_aa_tools function

--- common_utility.py
+++ common_utility.py
@@ -42,3 +42,26 @@ def run_dna_rna_tools(*args):
         else:
             return reverse_complement_result[0]
 
+
+def run_aa_tools(*args):
+    """
+    Main function for amino acid sequences processing.
+    Parameters: *args - amino acid sequences and operation.
+    Returns: List of results according to called operations.
+    """
+    if len(args) < 3:
+        return "Invalid input format: At least three arguments are required."
+
+    amino_acids = [arg.upper() for arg in args[:-2]]
+    pattern = args[-2].upper()
+    operation = args[-1]
+
+    if operation == "aa_pattern_position":
+        result = aa_pattern_position(amino_acids, pattern)
+    elif operation == "count_pattern_in_aa_sequences":
+        result = count_pattern_in_aa_sequences(amino_acids, pattern)
+    else:
+        return "Invalid operation"
+
+    return result
+
