fd468ce8829d5fad8873ffe316a56bd71e26ce7d
Maria Uzun <uzunmasha@gmail.com>
2023-10-08 12:25:50 +0300

    Add calculate_length function

--- modules/fastq.py
+++ modules/fastq.py
@@ -12,3 +12,20 @@ def calculate_gc(seqs: dict) -> list:
         gc_count = (sequence_value.count('G') + sequence_value.count('C')) / len(sequence_value) * 100
         gc_contents.append(gc_count)
     return gc_contents
+
+
+def calculate_length(seqs: dict) -> list:
+    """
+    Calculate length of given sequences.
+    As input takes a list of strings representing nucleic acid sequences.
+    Input should be obtained from a FASTQ dictionary.
+    Returns a list with lengths for each sequence.
+
+    """
+    seq_lenghts = []
+    for key, value in seqs.items():
+        sequence_value = value[0]
+        length_count = len(sequence_value)
+        seq_lenghts.append(length_count)
+
+    return seq_lenghts
