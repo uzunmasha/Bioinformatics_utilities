6b84cc5070b01fec9828659684f1dee984b98858
Maria Uzun <uzunmasha@gmail.com>
2023-10-08 12:26:59 +0300

    Add calculate_quality function

--- modules/fastq.py
+++ modules/fastq.py
@@ -29,3 +29,30 @@ def calculate_length(seqs: dict) -> list:
         seq_lenghts.append(length_count)
 
     return seq_lenghts
+
+
+def calculate_quality(seqs: dict) -> list:
+    """
+    Calculate quality of given sequences.
+    As input takes a list of strings representing nucleic acid's quality symbols according to Phred33 scale.
+    Input should be obtained from a FASTQ dictionary.
+    Returns a list with mean values of quality for each sequence.
+    """
+    QUALITY_VOCAB = {'!': 0, '"': 1, '#': 2, '$': 3, '%': 4, '&': 5, "'": 6, '(': 7, ')': 8, '*': 9, '+': 10,
+                     ',': 11, '-': 12, '.': 13, '/': 14, '0': 15, '1': 16, '2': 17, '3': 18, '4': 19, '5': 20,
+                     '6': 21, '7': 22, '8': 23, '9': 24, ':': 25, ';': 26, '<': 27, '=': 28, '>': 29, '?': 30,
+                     '@': 31, 'A': 32, 'B': 33, 'C': 34, 'D': 35, 'E': 36, 'F': 37, 'G': 38, 'H': 39, 'I': 40}
+
+    quality_contents = []
+    for key, value in seqs.items():
+        quality_string = value[1]
+        quality_sum = 0
+
+        for sign in quality_string:
+            quality_value = QUALITY_VOCAB.get(sign)
+            quality_sum += int(quality_value)
+
+        quality_count = quality_sum / len(quality_string)
+        quality_contents.append(quality_count)
+
+    return quality_contents
