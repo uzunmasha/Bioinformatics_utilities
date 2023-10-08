4a7f7d76b6d329dfe0f79313bcc9e88d96cea888
Maria Uzun <uzunmasha@gmail.com>
2023-10-08 12:29:48 +0300

    Import modules in main function

---
+++ common_utility.py
@@ -0,0 +1,3 @@
+from modules.dna_rna import check_nucleotides, transcribe, reverse, complement, reverse_complement
+from modules.amino_acids import aa_pattern_position, count_pattern_in_aa_sequences
+from modules.fastq import calculate_gc, calculate_length, calculate_quality
